61e7aa45112d0e95a70dfd4e4f9bf23fcb07f491
bayulaxana <bayulaxana@BayuLaxana.localdomain>
2020-04-24 21:02:12 +0700

    added a special function

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 use Phalcon\Db\Adapter\AdapterInterface;
 use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\Dispatcher;
+use Phalcon\Tag;
 
 class ControllerBase extends Controller
 {
@@ -16,5 +17,23 @@ class ControllerBase extends Controller
         
         // Global Styling
         $this->assets->addCss('css/global-style.css');
+    }
+
+    public function getFormattedFlashOutput(string $header, array $messageList)
+    {
+        $headerStr =    '<i class="close icon"></i>
+                        <div class="header">' . $header . '</div>';
+        if (count($messageList) == 1) {
+            return $headerStr . '<p>' . $messageList[0] . '</p>';
+        }
+        else {
+            $list = '<ul class="list">';
+            foreach ($messageList as $msg) {
+                $list .= '<li>' . $msg . '</li>';
+            }
+            $list .= '</ul>';
+            
+            return $headerStr . $list;
+        }
     }
 }
